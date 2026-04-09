9820b96de3a9382d6a206fe5a5a2b355f585e061
Joscha Moritz <joscha.fresmann@yahoo.de>
2026-04-09 14:13:37 +0200

    fix(belege): Fragment statt span, Leerzeichen als echter Text-Node

--- src/components/BelegeSatz.jsx
+++ src/components/BelegeSatz.jsx
@@ -1,11 +1,13 @@
+import { Fragment } from 'react'
+
 export default function BelegeSatz({ tokens }) {
   return (
     <p className="beleg-satz">
       {tokens.map((t, i) => (
-        <span key={i}>
-          {t.ws ? ' ' : ''}
+        <Fragment key={i}>
+          {t.ws && ' '}
           {t.hl ? <strong>{t.w}</strong> : t.w}
-        </span>
+        </Fragment>
       ))}
     </p>
   )
